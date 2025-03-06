872ad5c1c26edbc3f46e62841bc94dfc5db3d2a9
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-06 14:13:22 +0330

    feat : search logic added

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,47 +1,69 @@
-import React from 'react'
-import db from "./../../data/db.json"
-import HomeComponent from '@/Components/modules/HomeComponent'
+import React, { useEffect, useState } from "react";
+import db from "./../../data/db.json";
+import HomeComponent from "@/Components/modules/HomeComponent";
 
 export default function HomesPage() {
+
+
+  const [search,setSearch]=useState("")
+  const [homes,setHomes]=useState([...db.homes])
+
+  useEffect(()=>{
+
+    const newHome= db.homes.filter((h)=>h.title.includes(search))
+
+    setHomes(newHome)
+
+
+
+
+
+
+
+
+  },[search])
+
+
   return (
     <div class="home-section" id="houses">
-    <div class="home-filter-search">
+      <div class="home-filter-search">
         <div class="home-filter">
-            <select name="" id="">
-                <option value="" selected>انتخاب کنید</option>
-                <option value="">بر اساس قیمت</option>
-                <option value="">بر اساس تعداد اتاق</option>
-                <option value="">بر اساس ادرس</option>
-                <option value="">بر اساس اندازه</option>
-            </select>
+          <select name="" id="">
+            <option value="" selected>
+              انتخاب کنید
+            </option>
+            <option value="">بر اساس قیمت</option>
+            <option value="">بر اساس تعداد اتاق</option>
+            <option value="">بر اساس ادرس</option>
+            <option value="">بر اساس اندازه</option>
+          </select>
         </div>
         <div class="home-search">
-            <input type="text" placeholder="جستجو کنید" />
+          <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="جستجو کنید" />
         </div>
+      </div>
+      <div class="homes">
+        {homes.map((home) => {
+          return <HomeComponent key={home.id} {...home} />;
+        })}
+      </div>
+      <ul class="pagination__list">
+        <li class="pagination__item">
+          <a href="#" class="">
+            {" "}
+          </a>
+        </li>
+        <li class="pagination__item">
+          <a href="#" class="">
+            2
+          </a>
+        </li>
+        <li class="pagination__item active">
+          <a href="#" class="">
+            1
+          </a>
+        </li>
+      </ul>
     </div>
-    <div class="homes">
-
-
-      {
-        db.homes.map(home =>{
-          return(
-            <HomeComponent key={home.id} {...home}/>
-          )
-        })
-      }
-   
-
-
-
-
-
-
-    </div>
-    <ul class="pagination__list">
-        <li class="pagination__item"><a href="#" class="">  </a></li>
-        <li class="pagination__item"><a href="#" class="">2</a></li>
-        <li class="pagination__item active"><a href="#" class="">1</a></li>
-    </ul>
-</div>
-  )
+  );
 }
