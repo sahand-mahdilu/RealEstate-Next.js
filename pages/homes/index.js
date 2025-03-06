b88a9cf4566bb060a87352a1bb424dcfaa6342fd
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-06 18:12:33 +0330

    feat : sorting added

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -3,49 +3,87 @@ import db from "./../../data/db.json";
 import HomeComponent from "@/Components/modules/HomeComponent";
 
 export default function HomesPage() {
+  const [search, setSearch] = useState("");
+  const [homes, setHomes] = useState([...db.homes]);
+  const [sort, setSort] = useState("-1");
 
+  useEffect(() => {
+    const newHome = db.homes.filter((h) => h.title.includes(search));
 
-  const [search,setSearch]=useState("")
-  const [homes,setHomes]=useState([...db.homes])
+    setHomes(newHome);
+
+    console.log();
+  }, [search]);
 
   useEffect(()=>{
 
-    const newHome= db.homes.filter((h)=>h.title.includes(search))
 
-    setHomes(newHome)
+    switch(sort){
+      case "price":{
+
+        const newHomes = [...homes].sort((a,b)=> a.price - b.price)
+
+        setHomes(newHomes)
+
+      }
+      break;
+      case "roomCount":{
+        const newHomes = [...homes].sort((a,b)=> a.roomCount - b.roomCount)
+
+        setHomes(newHomes)
+
+      }
+      break;
+      case "meterage":{
+
+        const newHomes = [...homes].sort((a,b)=> a.meterage - b.meterage)
+
+        setHomes(newHomes)
+
+      }
+      break;
+      default :{
+
+      }
+    }
+   
 
 
 
-
-
-
-
-
-  },[search])
-
+  },[sort])
 
   return (
     <div class="home-section" id="houses">
       <div class="home-filter-search">
         <div class="home-filter">
-          <select name="" id="">
-            <option value="" selected>
+          <select defaultValue={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="-1" selected>
               انتخاب کنید
             </option>
-            <option value="">بر اساس قیمت</option>
-            <option value="">بر اساس تعداد اتاق</option>
-            <option value="">بر اساس ادرس</option>
-            <option value="">بر اساس اندازه</option>
+            <option value="price">بر اساس قیمت</option>
+            <option value="roomCount">بر اساس تعداد اتاق</option>
+            <option value="meterage">بر اساس اندازه</option>
           </select>
         </div>
         <div class="home-search">
-          <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="جستجو کنید" />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            type="text"
+            placeholder="جستجو کنید"
+          />
         </div>
       </div>
       <div class="homes">
-        {homes.map((home) => {
-          return <HomeComponent key={home.id} {...home} />;
-        })}
+        {homes.length > 0 ? (
+          homes.map((home) => {
+            return <HomeComponent key={home.id} {...home} />;
+          })
+        ) : (
+          <h1 className="font-semibold text-2xl text-red-500">
+            موردی یافت نشد
+          </h1>
+        )}
       </div>
       <ul class="pagination__list">
         <li class="pagination__item">
