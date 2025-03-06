447e8bfd3dbfac7ba66d15011ecfcb1a29dec3c3
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-06 12:10:24 +0330

    feat: homes content added

--- Components/template/index/Homes.js
+++ Components/template/index/Homes.js
@@ -1,7 +1,200 @@
-import React from 'react'
+import React from "react";
 
 export default function Homes() {
   return (
-    <div>Homes</div>
-  )
+    <div className="homes">
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <svg className="card__like">
+          <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+        </svg>
+        <div className="card__details">
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+          </svg>
+          <p className="card__text">مالدیو</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+          </svg>
+          <p className="card__text">4 اتاق</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-expand"></use>
+          </svg>
+          <p className="card__text">400 متر مربع</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-key"></use>
+          </svg>
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <svg className="card__like">
+          <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+        </svg>
+        <div className="card__details">
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+          </svg>
+          <p className="card__text">مالدیو</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+          </svg>
+          <p className="card__text">4 اتاق</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-expand"></use>
+          </svg>
+          <p className="card__text">400 متر مربع</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-key"></use>
+          </svg>
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <svg className="card__like">
+          <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+        </svg>
+        <div className="card__details">
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+          </svg>
+          <p className="card__text">مالدیو</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+          </svg>
+          <p className="card__text">4 اتاق</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-expand"></use>
+          </svg>
+          <p className="card__text">400 متر مربع</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-key"></use>
+          </svg>
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <svg className="card__like">
+          <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+        </svg>
+        <div className="card__details">
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+          </svg>
+          <p className="card__text">مالدیو</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+          </svg>
+          <p className="card__text">4 اتاق</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-expand"></use>
+          </svg>
+          <p className="card__text">400 متر مربع</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-key"></use>
+          </svg>
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <svg className="card__like">
+          <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+        </svg>
+        <div className="card__details">
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+          </svg>
+          <p className="card__text">مالدیو</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+          </svg>
+          <p className="card__text">4 اتاق</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-expand"></use>
+          </svg>
+          <p className="card__text">400 متر مربع</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-key"></use>
+          </svg>
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+      <div className="card">
+        <img src="img/gal-2.jpeg" alt="House 6" className="card__img" />
+        <h5 className="card__title">آپارتمان خانوادگی مدرن</h5>
+        <svg className="card__like">
+          <use xlinkHref="img/sprite.svg#icon-heart-full"></use>
+        </svg>
+        <div className="card__details">
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-map-pin"></use>
+          </svg>
+          <p className="card__text">مالدیو</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-profile-male"></use>
+          </svg>
+          <p className="card__text">4 اتاق</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-expand"></use>
+          </svg>
+          <p className="card__text">400 متر مربع</p>
+
+          <svg className="card__icon">
+            <use xlinkHref="img/sprite.svg#icon-key"></use>
+          </svg>
+          <p className="card__text">560 میلیون تومان</p>
+        </div>
+
+        <a href="singleHouse.html" className="btn btn-brown btn-card">
+          مشاهده ملک
+        </a>
+      </div>
+    </div>
+  );
 }
