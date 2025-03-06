5dcddf21b23a16279e870c6f5c292fb4c2561cb9
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-06 12:06:01 +0330

    feat : content added to story component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Features from "@/Components/template/index/Features";
+import Story from "@/Components/template/index/Story";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,9 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-<Features/>
-  )
+    <>
+      <Features />
+      <Story/>
+    </>
+  );
 }
