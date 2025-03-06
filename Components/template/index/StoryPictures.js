7187e7e5da240b54e78d1952724273d405ff4e12
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-06 12:01:06 +0330

    feat : storyPictures content added

--- Components/template/index/StoryPictures.js
+++ Components/template/index/StoryPictures.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function StoryPictures() {
   return (
-    <div>StoryPictures</div>
+    <div className="story__pictures">
+    <img
+        src="img/story-1.jpeg"
+        alt="Couple happy for new house"
+        className="story__img-1"
+    />
+    <img
+        src="img/story-2.jpeg"
+        alt="New house"
+        className="story__img-2"
+    />
+</div>
   )
 }
